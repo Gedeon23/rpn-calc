c18eba8755ac5ed7b679ab8aeb69d7c4678b1ef2
Gedeon <47500169+Gedeon23@users.noreply.github.com>
2024-09-22 03:04:08 +0200

    ADD stupid addition

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <curses.h>
 #include <ncurses.h>
+#include <string.h>
+#include <stdlib.h>
 
+void ExecuteAddition(WINDOW *win) {
+	char num1[100];
+	char num2[100];
+	move(getcury(win)-2,0);
+	winstr(win, num1);
+	move(getcury(win)+1,0);
+	winstr(win, num2);
+	move(getcury(win)-1,0);
+	printw("%d\n", atoi(num1) + atoi(num2));
+	clrtoeol();
+}
 
 
 
 int main() {	
 	int ch;
-	int content[100];
 
 	WINDOW *win = initscr();			/* Start curses mode 		*/
 	raw();				/* Line buffering disabled	*/
@@ -21,14 +33,15 @@ int main() {
 		} else if (ch == KEY_BACKSPACE) {
 			move(getcury(win), getcurx(win)-1);
 			delch();
-		// } else if (ch == KEY_F(1)) {
-		// 	winstr(win, content);
+		} else if (ch == 43) { /* if input == + */
+			ExecuteAddition(win);
 		} else {
 			printw("%c", ch);
 		}
 		refresh();			/* Print it on to the real screen */
 	}
 	endwin();			/* End curses mode		  */
+
 
 	return 0;
 }
